ad8cc9aa79d3d3f70e887511abaa81db7d3f5631
Diego Monti <diegmonti@gmail.com>
2017-12-18 13:32:19 +0100

    Implement coverage and precision

--- sequeval/evaluator.py
+++ sequeval/evaluator.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Evaluator:
 
     def __init__(self, training_set, test_set, items, k):
@@ -6,11 +9,61 @@ class Evaluator:
         self.items = items
         self.k = k
 
+        # Create the set of users
+        users = []
+
+        for sequence in training_set + test_set:
+            for rating in sequence:
+                users.append(rating)
+
+        self.users = sorted(list(set(users)))
+
     def coverage(self, recommender):
-        pass
+        total_items = 0
+
+        for user in self.users:
+            recommended_items = []
+
+            for item in self.items:
+                seed_rating = (item, user, 0)
+                recommended_sequence = recommender.generate(seed_rating, self.k)
+
+                for rating in recommended_sequence:
+                    recommended_items.append(rating[0])
+
+            total_items += len(set(recommended_items))
+
+        coverage = total_items / (len(self.items) * len(self.users))
+
+        return coverage
 
     def precision(self, recommender):
-        pass
+        precision = np.full(len(self.test_set), 0.0, dtype=float)
+
+        # For each sequence in the test set
+        for sequence_index, sequence in enumerate(self.test_set):
+            local_k = min(self.k, len(sequence) - 1)
+            hit = 0
+
+            recommended_sequence = recommender.generate(sequence[0], self.k)
+
+            # Create a list of reference items
+            reference_items = []
+
+            for rating in sequence[1:]:
+                reference_items.append(rating[0])
+
+            # For each rating in the recommended sequence
+            for rating in recommended_sequence:
+                # Check if it is also in the reference path
+                if rating[0] in reference_items:
+                    # Only the first time
+                    reference_items.remove(rating[0])
+                    hit += 1
+
+            precision[sequence_index] = hit / local_k
+
+        return precision.mean()
 
     def ndpm(self, recommender):
         pass
---
+++ tests/test_evaluator.py
@@ -0,0 +1,47 @@
+import unittest
+
+import sequeval
+import sequeval.baseline as baseline
+from .test_builder import sequences as training_set, items
+
+test_set = [[(1, 1, 0), (1, 1, 1), (2, 1, 2), (3, 1, 3)],
+            [(1, 1, 0), (1, 1, 1), (2, 1, 2), (1, 1, 3)]]
+
+
+class RecommenderTestSuite(unittest.TestCase):
+
+    def test_coverage(self):
+        recommender = baseline.MostPopularRecommender(training_set, items)
+        evaluator = sequeval.Evaluator(training_set, test_set, items, 3)
+        self.assertEqual(3 / 3, evaluator.coverage(recommender))
+        evaluator = sequeval.Evaluator(training_set, test_set, items, 2)
+        self.assertEqual(2 / 3, evaluator.coverage(recommender))
+
+    def test_precision(self):
+        recommender = baseline.MostPopularRecommender(training_set, items)
+        evaluator = sequeval.Evaluator(training_set, test_set, items, 3)
+        self.assertAlmostEqual(5 / 6, evaluator.precision(recommender))
+        evaluator = sequeval.Evaluator(training_set, test_set, items, 2)
+        self.assertEqual(1.0, evaluator.precision(recommender))
+
+    def test_ndpm(self):
+        pass
+
+    def test_diversity(self):
+        pass
+
+    def test_novelty(self):
+        pass
+
+    def test_serendipity(self):
+        pass
+
+    def test_confidence(self):
+        pass
+
+    def test_perplexity(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
