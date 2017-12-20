d40ef68c492634df4c588964a76f33b0acabfd6f
Diego Monti <diegmonti@gmail.com>
2017-12-20 12:20:53 +0100

    Load only users with at least a minimum number of ratings

--- sequeval/loader.py
+++ sequeval/loader.py
@@ -20,18 +20,20 @@ class Loader(ABC):
 
 class MovieLensLoader(Loader):
 
-    def __init__(self, limit, skip):
+    def __init__(self, min_ratings=0, threshold=None, skip=0):
         """
-        :param limit: The limit between positive and negative ratings.
+        :param min_ratings: The minimum number of ratings per each user.
+        :param threshold: The threshold between positive and negative ratings.
         :param skip: The number of rows to skip when reading the file.
         """
-        self.limit = limit
+        self.min_ratings = min_ratings
+        self.threshold = threshold
         self.skip = skip
 
     def load(self, file):
         """
         Load a CSV file with a MovieLens-like format.
-        Only the ratings higher than the limit will be considered.
+        Only the ratings higher than the threshold will be considered.
 
         :param file: The file path.
         :return: A list of ratings.
@@ -39,11 +41,26 @@ class MovieLensLoader(Loader):
         # Read the input file
         df_input = pandas.read_csv(file, names=['userId', 'itemId', 'rating', 'timestamp'], skiprows=self.skip)
 
-        # Select the ratings higher than the limit
-        df_filtered = df_input.loc[df_input['rating'] > self.limit]
+        # Select the ratings higher than the threshold
+        if self.threshold is not None:
+            df_filtered = df_input.loc[df_input['rating'] >= self.threshold]
+        else:
+            df_filtered = df_input
+
+        if self.min_ratings > 0:
+            # Count the ratings per each user
+            df_counter = df_filtered.groupby(['userId']).size().reset_index(name='counter')
+
+            # Select the users with more ratings than min_ratings
+            users_min_ratings = df_counter.loc[df_counter['counter'] >= self.min_ratings]['userId']
+
+            # Keep only the ratings by users_min_ratings
+            df_min_ratings = df_filtered.loc[df_filtered['userId'].isin(users_min_ratings)]
+        else:
+            df_min_ratings = df_filtered
 
         # Sort the by user and timestamp
-        df_sorted = df_filtered.sort_values(by=['userId', 'timestamp'])
+        df_sorted = df_min_ratings.sort_values(by=['userId', 'timestamp'])
 
         # Create a list of ratings
         ratings = []
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -16,24 +16,29 @@ ratings = [(1, 1, 0),
 
 class LoaderTestSuite(unittest.TestCase):
 
-    def test_movielens_plain(self):
+    def test_movielens(self):
         fake_input = "1,1,5,0\n1,2,5,5\n1,1,5,10\n1,3,5,30\n1,2,5,35,\n2,1,5,5\n2,1,5,30\n2,2,5,35\n2,2,5,40"
-        loader = sequeval.MovieLensLoader(3, None)
+        loader = sequeval.MovieLensLoader()
         self.assertEqual(ratings, loader.load(io.StringIO(fake_input)))
 
-    def test_movielens_order(self):
+    def test_movielens_ordering(self):
         fake_input = "1,2,5,5\n1,1,5,0\n1,1,5,10\n1,3,5,30\n1,2,5,35,\n2,1,5,5\n2,1,5,30\n2,2,5,35\n2,2,5,40"
-        loader = sequeval.MovieLensLoader(3, None)
+        loader = sequeval.MovieLensLoader()
         self.assertEqual(ratings, loader.load(io.StringIO(fake_input)))
 
-    def test_movielens_filter(self):
-        fake_input = "1,1,5,0\n1,2,5,5\n1,1,5,10\n1,3,5,30\n1,2,5,35,\n2,1,5,5\n2,1,5,30\n2,2,5,35\n2,2,3,40"
-        loader = sequeval.MovieLensLoader(3, None)
+    def test_movielens_min_ratings(self):
+        fake_input = "1,1,5,0\n1,2,5,5\n1,1,5,10\n1,3,5,30\n1,2,5,35,\n2,1,5,5\n2,1,5,30\n2,2,5,35\n2,2,5,40"
+        loader = sequeval.MovieLensLoader(min_ratings=5)
+        self.assertEqual(ratings[:5], loader.load(io.StringIO(fake_input)))
+
+    def test_movielens_threshold(self):
+        fake_input = "1,1,5,0\n1,2,5,5\n1,1,5,10\n1,3,5,30\n1,2,5,35,\n2,1,5,5\n2,1,5,30\n2,2,5,35\n2,2,2,40"
+        loader = sequeval.MovieLensLoader(threshold=3)
         self.assertEqual(ratings[0:8], loader.load(io.StringIO(fake_input)))
 
     def test_movielens_skip(self):
         fake_input = "1,1,5,0\n1,2,5,5\n1,1,5,10\n1,3,5,30\n1,2,5,35,\n2,1,5,5\n2,1,5,30\n2,2,5,35\n2,2,5,40"
-        loader = sequeval.MovieLensLoader(3, 1)
+        loader = sequeval.MovieLensLoader(skip=1)
         self.assertEqual(ratings[1:], loader.load(io.StringIO(fake_input)))
 
 
