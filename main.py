9984feb0b6d1f960f149feda92fd651968d63faa
Diego Monti <diegmonti@gmail.com>
2017-12-20 16:54:37 +0100

    Create the main script

--- main.py
+++ main.py
@@ -1,2 +1,71 @@
+import argparse
+
+import sequeval
+import sequeval.baseline as baseline
+
+
+def evaluation(compute, recommender, similarity):
+    coverage = compute.coverage(recommender)
+    precision = compute.precision(recommender)
+    ndpm = compute.ndpm(recommender)
+    diversity = compute.diversity(recommender, similarity)
+    novelty = compute.novelty(recommender)
+    serendipity = compute.serendipity(recommender)
+    confidence = compute.confidence(recommender)
+    perplexity = compute.perplexity(recommender)
+
+    print('\n' + str(coverage) + '\t' + str(precision) + '\t' + str(ndpm) +
+          '\t' + str(diversity) + '\t' + str(novelty) + '\t' + str(serendipity) +
+          '\t' + str(confidence) + '\t' + str(perplexity))
+
+
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='An evaluation framework for sequence-based recommender systems.')
+
+    parser.add_argument('file', type=str, help='file containing the ratings')
+    parser.add_argument('--min_ratings', type=int, default=5, help='minimum number of ratings per user and per item')
+    parser.add_argument('--delta_tau', type=int, default=7 * 24 * 3600, help='time interval to create the sequences')
+    parser.add_argument('--test_ratio', type=float, default=0.2, help='percentage of sequences in the test set')
+    parser.add_argument('--k', type=int, default=5, help='length of the recommended sequences')
+
+    args = parser.parse_args()
+
+    loader = sequeval.MovieLensLoader(min_ratings=args.min_ratings)
+    ratings = loader.load(args.file)
+
+    builder = sequeval.Builder(args.delta_tau)
+    sequences, items = builder.build(ratings)
+
+    profiler = sequeval.Profiler(sequences)
+    print("\nProfiler")
+    print("Users:", profiler.users())
+    print("Items:", profiler.items())
+    print("Ratings:", profiler.ratings())
+    print("Sequences:", profiler.sequences())
+    print("Sparsity:", profiler.sparsity())
+    print("Length:", profiler.sequence_length())
+
+    splitter = sequeval.TimestampSplitter(args.test_ratio)
+    training_set, test_set = splitter.split(sequences)
+    print("\nSplitter")
+    print("Training set:", len(training_set))
+    print("Test set:", len(test_set))
+
+    evaluator = sequeval.Evaluator(training_set, test_set, items, args.k)
+    cosine = sequeval.CosineSimilarity(training_set, items)
+
+    print("\nMost popular")
+    most_popular = baseline.MostPopularRecommender(training_set, items)
+    evaluation(evaluator, most_popular, cosine)
+
+    print("\nRandom")
+    random = baseline.RandomRecommender(training_set, items)
+    evaluation(evaluator, random, cosine)
+
+    print("\nUnigram")
+    unigram = baseline.UnigramRecommender(training_set, items)
+    evaluation(evaluator, unigram, cosine)
+
+    print("\nBigram")
+    bigram = baseline.BigramRecommender(training_set, items)
+    evaluation(evaluator, bigram, cosine)
