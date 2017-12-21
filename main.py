2424c1caececc4a659765541fbd67c37b7752df5
Diego Monti <diegmonti@gmail.com>
2017-12-21 14:35:52 +0100

    Human time interval parsing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 
+import pytimeparse
+
 import sequeval
 import sequeval.baseline as baseline
 
@@ -23,17 +25,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='An evaluation framework for sequence-based recommender systems.')
 
     parser.add_argument('file', type=str, help='file containing the ratings')
-    parser.add_argument('--min_ratings', type=int, default=5, help='minimum number of ratings per user and per item')
-    parser.add_argument('--delta_tau', type=int, default=7 * 24 * 3600, help='time interval to create the sequences')
-    parser.add_argument('--test_ratio', type=float, default=0.2, help='percentage of sequences in the test set')
+    parser.add_argument('--ratings', type=int, default=5, help='minimum number of ratings per user and per item')
+    parser.add_argument('--delta', type=str, default='8 hours', help='time interval to create the sequences')
+    parser.add_argument('--ratio', type=float, default=0.2, help='percentage of sequences in the test set')
     parser.add_argument('--k', type=int, default=5, help='length of the recommended sequences')
 
     args = parser.parse_args()
 
-    loader = sequeval.MovieLensLoader(min_ratings=args.min_ratings)
+    loader = sequeval.MovieLensLoader(min_ratings=args.ratings)
     ratings = loader.load(args.file)
 
-    builder = sequeval.Builder(args.delta_tau)
+    builder = sequeval.Builder(pytimeparse.parse(args.delta))
     sequences, items = builder.build(ratings)
 
     print("# Profiler")
@@ -46,12 +48,15 @@ if __name__ == '__main__':
     print("Length:", profiler.sequence_length())
 
     print("\n# Splitter")
-    splitter = sequeval.TimestampSplitter(args.test_ratio)
+    splitter = sequeval.TimestampSplitter(args.ratio)
     training_set, test_set = splitter.split(sequences)
     print("Training set:", len(training_set))
     print("Test set:", len(test_set))
 
     print("\n# Evaluator")
+    print("Recommender" + '\t' + "Coverage" + '\t' + "Precision" +
+          '\t' + "nDPM" + '\t' + "Diversity" + '\t' + "Novelty" +
+          '\t' + "Serendipity" + '\t' + "Confidence" + '\t' + "Perplexity")
     evaluator = sequeval.Evaluator(training_set, test_set, items, args.k)
     cosine = sequeval.CosineSimilarity(training_set, items)
 
