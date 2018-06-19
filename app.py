f3c9beb23bc1535defed54758d42db5c2650cd43
Diego Monti <diegmonti@gmail.com>
2018-06-19 16:32:50 +0200

    Add web interface

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, json, request
+
+import sequeval
+import sequeval.baseline as baseline
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+def parse(value):
+    if value == float('inf'):
+        return 'Inf'
+    if isinstance(value, float):
+        return str("%.2f" % round(value, 4))
+    return str(value)
+
+
+def evaluation(compute, recommender, similarity):
+    return [recommender.name,
+            parse(compute.coverage(recommender)),
+            parse(compute.precision(recommender)),
+            parse(compute.ndpm(recommender)),
+            parse(compute.diversity(recommender, similarity)),
+            parse(compute.novelty(recommender)),
+            parse(compute.serendipity(recommender)),
+            parse(compute.confidence(recommender)),
+            parse(compute.perplexity(recommender))]
+
+
+@app.route("/run", methods=['GET'])
+def run():
+    _user_ratings = int(request.args.get('user-ratings'))
+    _item_ratings = int(request.args.get('item-ratings'))
+    _splitter = request.args.get('splitter')
+    _ratio = float(request.args.get('ratio')) / 100
+    _k = int(request.args.get('length'))
+
+    loader = sequeval.MovieLensLoader(user_ratings=_user_ratings, item_ratings=_item_ratings)
+    ratings = loader.load('datasets/yes_reduced.csv')
+
+    builder = sequeval.Builder('1000 s')
+    sequences, items = builder.build(ratings)
+
+    profiler = sequeval.Profiler(sequences)
+    response = {'profiler': {'users': profiler.users(),
+                             'items': profiler.items(),
+                             'ratings': profiler.ratings(),
+                             'sequences': profiler.sequences(),
+                             'sparsity': parse(profiler.sparsity()),
+                             'length': parse(profiler.sequence_length())}}
+
+    if _splitter == 'random':
+        splitter = sequeval.RandomSplitter(_ratio)
+    else:
+        splitter = sequeval.TimestampSplitter(_ratio)
+    training_set, test_set = splitter.split(sequences)
+    response['splitter'] = {'training': len(training_set),
+                            'test': len(test_set)}
+
+    evaluator = sequeval.Evaluator(training_set, test_set, items, _k)
+    cosine = sequeval.CosineSimilarity(training_set, items)
+
+    response['evaluator'] = []
+
+    most_popular = baseline.MostPopularRecommender(training_set, items)
+    response['evaluator'].append(evaluation(evaluator, most_popular, cosine))
+
+    random = baseline.RandomRecommender(training_set, items)
+    response['evaluator'].append(evaluation(evaluator, random, cosine))
+
+    unigram = baseline.UnigramRecommender(training_set, items)
+    response['evaluator'].append(evaluation(evaluator, unigram, cosine))
+
+    bigram = baseline.BigramRecommender(training_set, items)
+    response['evaluator'].append(evaluation(evaluator, bigram, cosine))
+
+    return json.dumps(response)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/sequeval.js
@@ -0,0 +1,57 @@
+$(function () {
+    $('#run-evaluator').submit(function () {
+        $('.btn').prop('disabled', true);
+        $('#result').empty();
+
+        $.getJSON('/run', $(this).serialize(), function (json) {
+            let profiler = json['profiler'];
+            let result = $('#result');
+            let h3 = $('<h3>').text('Profiler');
+            result.append(h3);
+            let ul = $('<ul>');
+            result.append(ul);
+            ul.append($('<li>').text('Users: ' + profiler['users']));
+            ul.append($('<li>').text('Items: ' + profiler['items']));
+            ul.append($('<li>').text('Ratings: ' + profiler['ratings']));
+            ul.append($('<li>').text('Sequences: ' + profiler['sequences']));
+            ul.append($('<li>').text('Sparsity: ' + profiler['sparsity']));
+            ul.append($('<li>').text('Length: ' + profiler['length']));
+
+            let splitter = json['splitter'];
+            h3 = $('<h3>').text('Splitter');
+            result.append(h3);
+            ul = $('<ul>');
+            result.append(ul);
+            ul.append($('<li>').text('Training set: ' + splitter['training']));
+            ul.append($('<li>').text('Test set: ' + splitter['test']));
+
+            let evaluator = json['evaluator'];
+            h3 = $('<h3>').text('Evaluator');
+            result.append(h3);
+            let table = $('<table>').addClass('table');
+            result.append(table);
+            let tr = $('<tr>');
+            table.append(tr);
+            tr.append($('<th>').text('Algorithm'));
+            tr.append($('<th>').text('Coverage'));
+            tr.append($('<th>').text('Precision'));
+            tr.append($('<th>').text('nDPM'));
+            tr.append($('<th>').text('Diversity'));
+            tr.append($('<th>').text('Novelty'));
+            tr.append($('<th>').text('Serendipity'));
+            tr.append($('<th>').text('Confidence'));
+            tr.append($('<th>').text('Perplexity'));
+            for (let i = 0; i < evaluator.length; i++) {
+                let tr = $('<tr>');
+                table.append(tr);
+                tr.append($('<th>').text(evaluator[i][0]));
+                for (let j = 1; j < evaluator[i].length; j++) {
+                    tr.append($('<td>').text(evaluator[i][j]));
+                }
+            }
+
+            $('.btn').prop('disabled', false);
+        });
+        return false;
+    });
+});
