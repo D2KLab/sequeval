bf73d36ec00b14f926abe1df64b4bda4fe797821
Diego Monti <diegmonti@gmail.com>
2018-10-01 14:16:33 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sequeval',
-    version='1.1.0',
+    version='1.1.1',
     packages=['sequeval',
               'sequeval.baseline'],
     url='https://github.com/D2KLab/sequeval',
